09fa2406e84f4b586aa5568cbdb1e0642e5e3374
Barathpalanisamy <barath@thirvusoft.co.in>
2023-08-16 19:35:15 +0530

    program agenda

--- e_desk/e_desk/doctype/program_agenda/program_agenda.js
+++ e_desk/e_desk/doctype/program_agenda/program_agenda.js
@@ -2,7 +2,24 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Program Agenda', {
-	// refresh: function(frm) {
+	validate: function(frm) {
 
-	// }
+        var child_table = frm.doc.agenda_details || [];
+        child_table.sort(function(a, b) {
+            return frappe.datetime.get_diff(a.from_time, b.from_time);
+        });
+
+
+		for (var i = 0; i < child_table.length - 1; i++) {
+            var current_row = child_table[i];
+            var next_row = child_table[i + 1];
+
+            if (current_row['to_time'] >= next_row['from_time']) {
+                frappe.msgprint(__('Time intervals should not overlap.'));
+                frappe.validated = false;
+                return;
+            }
+        }
+    }
 });
+
